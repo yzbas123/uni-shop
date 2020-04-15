195977b046914defb5a42b9e051bf7122ae17428
yzbas123 <5314264343@qq.com>
2020-04-15 23:21:44 +0800

    首页轮播图完成

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
-	<view>
-		首页
+	<view class="home">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular>
+			<swiper-item v-for="item in swiperDatas" :key='item.key'>
+				<navigator :url="item.url">
+					<image :src="item.img"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,15 +14,52 @@
 	export default {
 		data() {
 			return {
-				
+				swiperDatas: []
 			}
 		},
+		onLoad() {
+			this.getSwiperDatas()
+		},
 		methods: {
-			
+			// 获取轮播图数据
+			getSwiperDatas() {
+				console.log('获取轮播图数据');
+				uni.request({
+					url: 'http://localhost:8082/api/getlunbo',
+					method: 'GET',
+					data: {},
+					success: res => {
+						console.log(res.data);
+						// 将轮播图数据进行缓存
+						if (res.data.status !== 0) {
+							// 获取失败,提示
+							return uni.showToast({
+								title: '获取数据失败'
+							});
+						}
+						this.swiperDatas = res.data.message
+					},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+.home{
+	swiper{
+		width: 750upx;
+		height: 380rpx;
+		navigator{
+			width: 100%;
+			height: 100%;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+}
 </style>
