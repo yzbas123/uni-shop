9f72031bd375caab478f84e3e35a5ff3d2b4d7cb
yzbas123 <5314264343@qq.com>
2020-04-21 22:21:52 +0800

    完成资讯详情页

--- pages/news-detail/news-detail.vue
+++ pages/news-detail/news-detail.vue
@@ -1,6 +1,15 @@
 <template>
-	<view>
-		资讯详情pages
+	<view class="news-detail">
+		<!-- 标题 -->
+		<view class="title">{{detailInfo.title}}</view>
+		<!-- 其他信息 -->
+		<view class="other">
+			<!-- 创建时间 -->
+			<text>发表时间:{{detailInfo.add_time|dateFormate}}</text>
+			<!-- 点击量 -->
+			<text>点击量:{{detailInfo.click}}</text>
+		</view>
+		<rich-text :nodes="detailInfo.content" class="content"></rich-text>
 	</view>
 </template>
 
@@ -24,16 +33,39 @@
 				});
 			}
 			// 缓存数据
-			this.detailInfos = res.data.message
+			this.detailInfo = res.data.message[0]
 		},
 		data() {
 			return {
-				detailInfos: []
+				detailInfo: {}
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.news-detail{
+	box-sizing: border-box;
+	padding: 10rpx 20rpx;
+	.title{
+		text-align: center;
+		font-size: 36rpx;
+		font-weight: 700;
+	}
+	.other{
+		display: flex;
+		padding: 0 80rpx;
+		justify-content: space-between;
+		margin: 20rpx 0;
+		font-size: 22rpx;
+		color: #333;
+	}
+	.content{
+		font-size: 28rpx;
+		line-height: 1.5em;
+		strong{
+			line-height: 2em;
+		}
+	}
+}
 </style>
